ba279ed4598ec69d8bdaa9a6a8245ddfab84fcea
testinginterviewmeow <140732876+testinginterviewmeow@users.noreply.github.com>
2023-08-19 16:51:57 +0200

    contants.py

---
+++ contants.py
@@ -0,0 +1,4 @@
+char = 7 #Characteristic of the field.
+poly=[[0],[2,3,0,0],[1,1,0,1],[1,0,1,1]] #This is the polynomial. Every vector is a monomial: The first value is the coefficient and the other values are the degree of each variable. For example, the monomial 3X_1(x_2)^3x_3 is represented by (3,1,3,1). 
+dimension = 4 #maximal degree of each variable
+
